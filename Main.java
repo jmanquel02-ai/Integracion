db2752d291a1a110e6935ed64d1f71f5d954517f
jmanquel02-ai <j.manquel02@ufromail.cl>
2026-03-25 11:06:23 -0300

    Subiendo mi primer programa en java

---
+++ Main.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hola, soy Jonathan y este es mi primer hola mundo en java");
+    }
+}
